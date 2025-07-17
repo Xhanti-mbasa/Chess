1737194caa5faa4c8976adf6ad90fe1387def5d0
Xhanti <xhantilomzimbasadaka@gmail.com>
2025-07-17 13:41:11 +0200

    Update chess.py

--- chess.py
+++ chess.py
@@ -1,11 +1,1 @@
-def binary_search(numbers, target):
-    if target:
-        return target
-    else:
-        return index
 
-        index = -1
-
-numbers = [1, 2, 3, 4, 5]
-target = numbers.index(7)
-print(binary_search(numbers,target))
